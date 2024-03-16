464463d1eac60a0e54cb882554e06054db87a19f
Rahu-wajid <159697017+Rahu-wajid@users.noreply.github.com>
2024-03-16 03:54:44 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("   BMI CALCULATOR    ");
+var weightInkg = 64;
+var heightInMeters = 1.75;
+var BMI = weightInkg / (heightInMeters * weightInkg);
+console.log("SO YOUR BMI IS ".concat(BMI));
